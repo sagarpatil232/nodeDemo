00e4ea2129eee61cc468f9e3ea0d738ae6abe0b7
sagarp007 <sagarpatil232@gmail.com>
2022-02-06 18:04:28 +0530

    added tutorials code

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
+//const cors = require("cors");
+// var corsOptions = {
+//   origin: "http://localhost:8081"
+// };
+
+// app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
 var filetest = require('./routes/filetest')
+var tutorial = require('./routes/tutorial')
 
 app.use('/filetest', filetest)
+app.use('/tutorial', tutorial)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
---
+++ routes/tutorial.js
@@ -0,0 +1,31 @@
+var express = require('express')
+var router = express.Router()
+//module.exports = app => {
+    const tutorials = require("../controllers/tutorial.js");
+  
+  
+    // Create a new Tutorial
+    router.post("/", tutorials.create);
+  
+    // Retrieve all Tutorials
+    router.get("/", tutorials.findAll);
+  
+    // Retrieve all published Tutorials
+    router.get("/published", tutorials.findAllPublished);
+  
+    // Retrieve a single Tutorial with id
+    router.get("/:id", tutorials.findOne);
+  
+    // Update a Tutorial with id
+    router.put("/:id", tutorials.update);
+  
+    // Delete a Tutorial with id
+    router.delete("/:id", tutorials.delete);
+  
+    // Delete all Tutorials
+    router.delete("/", tutorials.deleteAll);
+  
+  //   app.use('/api/tutorials', router);
+  // };
+
+  module.exports = router
